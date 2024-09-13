19d42791670c896e88b29d5597e903ae41b3922a
alisa <alisa@alisaqaq.moe>
2024-09-13 15:18:49 +0800

    feat: add npgsql telemetry

--- src/owoBot.EntityFrameworkCore/Extension.cs
+++ src/owoBot.EntityFrameworkCore/Extension.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Npgsql;
+using OpenTelemetry.Metrics;
 
 namespace owoBot.EntityFrameworkCore;
 
@@ -8,5 +10,33 @@ public static class Extension
     public static void AddEntityFrameworkCore(this IHostApplicationBuilder builder)
     {
         builder.Services.AddDbContext<OWODbContext>();
+
+        builder.Services
+            .AddHealthChecks()
+            .AddDbContextCheck<OWODbContext>("npgsql");
+
+        double[] secondsBuckets = [0, 0.005, 0.01, 0.025, 0.05, 0.075, 0.1, 0.25, 0.5, 0.75, 1, 2.5, 5, 7.5, 10];
+
+        builder.Services
+            .AddOpenTelemetry()
+            .WithMetrics(metrics =>
+            {
+                metrics
+                    .AddMeter("Npgsql")
+                    .AddView("db.client.commands.duration",
+                        new ExplicitBucketHistogramConfiguration
+                        {
+                            Boundaries = secondsBuckets
+                        })
+                    .AddView("db.client.connections.create_time",
+                        new ExplicitBucketHistogramConfiguration
+                        {
+                            Boundaries = secondsBuckets
+                        });
+            })
+            .WithTracing(tracer =>
+            {
+                tracer.AddNpgsql();
+            });
     }
 }
